ff8e5ee4d295aaa073c32489a14564f84b0e5199
ValentainFomkin <valentain1625@mail.ru>
2022-09-05 13:51:59 +0300

    Concatenating Strings with the Plus Equals Operator

---
+++ 18.js
@@ -0,0 +1,7 @@
+/* Построить myStrиз строк This is the start.и This is the end.с помощью +оператора.
+ Не забудьте включить пробел между двумя строками.*/
+const myStr = 'This is the start. ' + 'This is the end.'; // Change this line
+console.log(myStr);
+
+let myStrr = 'This is the first sentence. ';
+myStrr += 'This is the second sentence.';
