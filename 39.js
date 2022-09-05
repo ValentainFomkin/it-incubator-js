5da8f2bad6048fb07f4bcc1d5ebdfbcb10d28320
ValentainFomkin <valentain1625@mail.ru>
2022-09-06 00:18:46 +0300

    boolean

---
+++ 39.js
@@ -0,0 +1,8 @@
+/* Измените welcomeToBooleansфункцию, чтобы она возвращалась, trueа не falseпри нажатии кнопки запуска.*/
+function welcomeToBooleans() {
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
