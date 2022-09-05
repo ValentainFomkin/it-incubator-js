1fa51bf46ad8bba4ae45e218654894f1bbd57443
ValentainFomkin <valentain1625@mail.ru>
2022-09-06 00:22:18 +0300

    if. true of false

---
+++ 40.js
@@ -0,0 +1,8 @@
+/* Создайте ifоператор внутри функции для возврата Yes, that was true,
+ если параметр wasThatTrueесть, trueи возврата в No, that was falseпротивном случае.*/
+function trueOrFalse(wasThatTrue) {
+  if (wasThatTrue) {
+    return 'Yes, that was true';
+  }
+  return 'No, that was false';
+}
