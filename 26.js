2d1649d611d099215c0564e1f3f99ef2bd03789e
ValentainFomkin <valentain1625@mail.ru>
2022-09-05 21:56:13 +0300

    .pop() onto the end array

---
+++ 26.js
@@ -0,0 +1,8 @@
+// Используйте .pop()функцию, чтобы удалить последний элемент myArrayи
+// присвоить выскочившее значение новой переменной, removedFromMyArray.
+const myArray = [
+  ['John', 23],
+  ['cat', 2],
+];
+let removedFromMyArray = myArray.pop();
+// Only change code below this line
