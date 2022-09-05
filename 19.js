afc7ed83ed8f8a69f7ce520ac6995d08911b66a8
ValentainFomkin <valentain1625@mail.ru>
2022-09-05 13:59:13 +0300

    Добавление переменных к строкам

--- 19.js
+++ 19.js
@@ -2,3 +2,7 @@
 // Only change code below this line
 const myName = 'Valentain';
 const myStr = 'My name is ' + myName + ' and I am well!';
+
+const someAdjective = 'JavaScript';
+let myStrr = 'Learning to code is ';
+myStrr += someAdjective;
