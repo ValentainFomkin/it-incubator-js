70fab11f90d5886b61f0716b5ea4ef5090f7294f
ValentainFomkin <valentain1625@mail.ru>
2022-09-05 14:42:06 +0300

    created array and concatenation strings

---
+++ 22.js
@@ -0,0 +1,14 @@
+const myNoun = 'dog';
+const myAdjective = 'big';
+const myVerb = 'ran';
+const myAdverb = 'quickly';
+const wordBlanks =
+  'I love the ' +
+  myNoun +
+  ' because he ' +
+  myAdjective +
+  ' and i ' +
+  myVerb +
+  ' asdasas ' +
+  myAdverb;
+console.log(wordBlanks);
