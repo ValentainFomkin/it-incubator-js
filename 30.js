ab73ac5c34be9f808b70cce763b6a9f9adc02f0d
ValentainFomkin <valentain1625@mail.ru>
2022-09-05 22:12:43 +0300

    functions

---
+++ 30.js
@@ -0,0 +1,6 @@
+/* Создайте вызываемую функцию, reusableFunctionкоторая выводит строку Hi Worldна консоль разработчика.
+Вызов функции.*/
+function reusableFunction() {
+  console.log('Hi World');
+}
+reusableFunction();
