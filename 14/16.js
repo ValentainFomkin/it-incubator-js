95b9e5a17692596e64da95c82dafc3e147e45fd3
ValentainFomkin <valentain1625@mail.ru>
2022-09-05 12:54:03 +0300

    using backslashes Escaping Literal Quotes in Strings

---
+++ 14/16.js
@@ -0,0 +1,2 @@
+//Используйте обратную косую черту , чтобы присвоить строку myStrпеременной, чтобы, если вы выведете ее на консоль, вы увидели:
+const myStr = 'I am a "double quoted" string inside "double quotes".'; // Change this line
