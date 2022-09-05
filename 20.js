303e635598dfb19a2d79ca8a9acf82d7f97f66b7
ValentainFomkin <valentain1625@mail.ru>
2022-09-05 14:16:21 +0300

    length for string

---
+++ 20.js
@@ -0,0 +1,8 @@
+//Найдите длину строки
+// Setup
+let lastNameLength = 0;
+const lastName = 'Lovelacedadasdasdasdasdas';
+
+// Only change code below this line
+lastNameLength = lastName.length;
+console.log(lastNameLength);
