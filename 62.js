37cdee4313f8dd8de6f619a8d59ab7339c6affb5
ValentainFomkin <valentain1625@mail.ru>
2022-09-06 16:47:54 +0300

    return bulean

---
+++ 62.js
@@ -0,0 +1,10 @@
+/* Исправьте функцию isLess, чтобы удалить if/elseоператоры.*/
+
+function isLess(a, b) {
+  // Only change code below this line
+  return a < b;
+
+  // Only change code above this line
+}
+
+isLess(10, 15);
