00bebe8f6c01d45885f2fae859532b3c4943068a
ValentainFomkin <valentain1625@mail.ru>
2022-09-06 18:07:10 +0300

    change name in object

---
+++ 69.js
@@ -0,0 +1,11 @@
+/* Обновите myDogсвойство имени объекта. Давайте изменим ее имя с Coderна Happy Coder.
+ Вы можете использовать либо точку, либо квадратную нотацию.*/
+// Setup
+const myDog = {
+  name: 'Coder',
+  legs: 4,
+  tails: 1,
+  friends: ['freeCodeCamp Campers'],
+};
+myDog['name'] = 'Happy Coder';
+// Only change code below this line
