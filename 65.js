a0dd24283911872b4de18eb69eec23784dc03a0b
ValentainFomkin <valentain1625@mail.ru>
2022-09-06 17:48:48 +0300

    created object

---
+++ 65.js
@@ -0,0 +1,12 @@
+/*Создайте объект, представляющий вызываемую собаку, myDogкоторый содержит свойства name(строку) legs, tailsи friends.
+
+Вы можете установить для этих свойств объекта любые значения, которые вы хотите, если nameэто строка, числа и legsмассив .tailsfriends*/
+const myDog = {
+  // Only change code below this line
+  name: 'Bobick',
+  legs: 4,
+  tails: 1,
+  friends: ['cats', 'human'],
+
+  // Only change code above this line
+};
