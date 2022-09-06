4de5a9758955dcfecad344edc1fa420706b12813
ValentainFomkin <valentain1625@mail.ru>
2022-09-06 17:55:47 +0300

    read object using dote notation

---
+++ 66.js
@@ -0,0 +1,14 @@
+/*Считайте значения свойств testObj, используя точечную нотацию. 
+Установите переменную hatValue равной свойству объекта hat и
+ установите переменную shirtValue равной свойству объекта shirt.*/
+
+// Setup
+const testObj = {
+  hat: 'ballcap',
+  shirt: 'jersey',
+  shoes: 'cleats',
+};
+
+// Only change code below this line
+const hatValue = testObj.hat; // Change this line
+const shirtValue = testObj.shirt; // Change this line
