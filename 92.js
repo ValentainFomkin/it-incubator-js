c9696be08caeffe7ab2307448ec5195ba18fb09b
ValentainFomkin <valentain1625@mail.ru>
2022-09-09 20:26:08 +0300

    replace(/&/g, &amp

--- 92.js
+++ 92.js
@@ -8,12 +8,11 @@
  */
 
 function htmlspecialchars(formData) {
-  let result = '';
-  for (let i = 0; i < formData.length; i++) {
-    if (formData[i] == '<') {
-      formData[i].replace('<', '&lt;');
-      return (result += formData);
-    }
-  }
+  return formData
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
 }
+
 console.log(htmlspecialchars('<h2>Hello World</h2>'));
