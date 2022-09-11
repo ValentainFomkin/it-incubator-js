52ffc8163801b29c76ac9ae7849d2f9c922ef0aa
ValentainFomkin <valentain1625@mail.ru>
2022-09-12 00:23:09 +0300

    Spinning Rings

---
+++ 156.js
@@ -0,0 +1,13 @@
+function spinningRings(innerMax, outerMax) {
+  let inner = 0,
+    outer = 0,
+    step = 0;
+  do {
+    step++;
+    inner--;
+    outer++;
+    if (inner < 0) inner = innerMax;
+    if (outer > outerMax) outer = 0;
+  } while (inner != outer);
+  return step;
+}
