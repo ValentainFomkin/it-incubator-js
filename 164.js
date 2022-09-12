afbcbc5aeb4daaace38a0dbacab4d54fe58748ca
ValentainFomkin <valentain1625@mail.ru>
2022-09-12 20:53:37 +0300

    recursion 2.0

--- 164.js
+++ 164.js
@@ -6,3 +6,12 @@ function multiply(arr, n) {
   }
 }
 console.log(multiply([2, 3, 4, 5], 3));
+
+let res = 1;
+function nFact(n) {
+  if (n === 0) return;
+  res = res * n;
+  nFact(n - 1);
+}
+nFact(5);
+console.log(res);
