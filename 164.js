b8d3364318abb084ff247a251ee6e6e00e7b0025
ValentainFomkin <valentain1625@mail.ru>
2022-09-13 12:58:46 +0300

    recursive function that returns an array containing the numbers 1 through n.

--- 164.js
+++ 164.js
@@ -2,7 +2,7 @@ function multiply(arr, n) {
   if (n <= 0) {
     return 1;
   } else {
-    return multiply(arr, n - 1);
+    return multiply(arr, n - 1) * arr[n];
   }
 }
 console.log(multiply([2, 3, 4, 5], 3));
---
+++ 173.js
@@ -0,0 +1,21 @@
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
+}
+console.log(countdown(10));
+
+function countup(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countup(n - 1);
+    countArray.push(n);
+    return countArray;
+  }
+}
+console.log(countup(5));
