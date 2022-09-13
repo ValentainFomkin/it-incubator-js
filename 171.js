33f59a9169444c21105a49d65006a752c1570ba7
ValentainFomkin <valentain1625@mail.ru>
2022-09-13 12:28:38 +0300

    ternarniy operator if else

---
+++ 171.js
@@ -0,0 +1,8 @@
+/*Используйте условный оператор в checkEqualфункции, чтобы проверить, 
+равны ли два числа или нет. Функция должна возвращать либо строку Equal, либо строку Not Equal.*/
+
+function checkEqual(a, b) {
+  return a === b ? 'Equal' : 'Not Equal';
+}
+
+checkEqual(1, 2);
